d79ad11c299f3470118bcb411aada019b77bf23b
erica7dev <erikaisa7dev@gmail.com>
2022-08-02 17:56:37 -0300

    config firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'pokedex-angular-d6144',
+    appId: '1:669449001093:web:c9326e03654b966db7b1ec',
+    storageBucket: 'pokedex-angular-d6144.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyB3AoEytsLlJSRoKSm2_q65pYInXOUjR_s',
+    authDomain: 'pokedex-angular-d6144.firebaseapp.com',
+    messagingSenderId: '669449001093',
+  },
   production: true
 };
